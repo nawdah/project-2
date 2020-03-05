2558dccdec16b22282ef7138ba2d37727b77683c
Dan Bishop <56052905+bishdj22@users.noreply.github.com>
2020-03-04 22:39:58 -0500

    DB commit 3/4 work

---
+++ SQL/cosmetic_tbl_build.sql
@@ -0,0 +1,12 @@
+CREATE TABLE "cosmetics" (
+	"index" int,
+	"brand" varchar(30),
+	"product" varchar(30),
+	"product_type" varchar(30),
+	"price" int,
+	"rating" int,
+	"details" varchar(30),
+	"ingredients" varchar(30),
+	"url" varchar(30),
+	"store" varchar(30)
+);

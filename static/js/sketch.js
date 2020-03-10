61f71ae266e17b851a99b478594a0829267c7915
nawdah <nadaibrhm96@gmail.com>
2020-03-10 02:00:11 -0400

    adding js files update

--- static/js/sketch.js
+++ static/js/sketch.js
@@ -1,7 +1,29 @@
 function setup(){
-    createCanvas(640,360);
+
+
+    //OKAY SORTA WORKING???? JUST NOT ON THE FACE ???
+    var faceClick = createA("#face", "face");
+
+    var face = document.querySelector("#face");
+    face.addEventListener("click", function(){
+        createCanvas(500,500);
+        background(0);
+
+    })
+
+    faceClick.class("face");
+
+
+
+    // var face = document.querySelector("#face");
+    // face.addEventListener("click", function(){
+    //     createCanvas(500,500);
+
+    //     background(0);
+    // })
+
+    // face.class(".float-right");
+    
+    
 }
 
-function draw(){
-    rect(600, 4, 50, 80);
-}

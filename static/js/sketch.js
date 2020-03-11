d3b7cf34367cc87ee4b3a17ae8623ac78433a8be
nawdah <nadaibrhm96@gmail.com>
2020-03-10 20:59:12 -0400

    adding

--- static/js/sketch.js
+++ static/js/sketch.js
@@ -1,29 +1,73 @@
-function setup(){
+let sketch = function(p) {
+    p.setup = function(){
+      p.createCanvas(100, 100);
+      p.background(15);
+    }
+  };
+new sketch(sketch, window.document.getElementById('container'));
 
 
-    //OKAY SORTA WORKING???? JUST NOT ON THE FACE ???
-    var faceClick = createA("#face", "face");
+// let blush, foundation, eyeshadow, eyeliner;
 
-    var face = document.querySelector("#face");
-    face.addEventListener("click", function(){
-        createCanvas(500,500);
-        background(0);
+// //var img;
 
-    })
-
-    faceClick.class("face");
-
-
-
-    // var face = document.querySelector("#face");
-    // face.addEventListener("click", function(){
-    //     createCanvas(500,500);
-
-    //     background(0);
-    // })
-
-    // face.class(".float-right");
+// function setup(){
     
-    
-}
 
+// }
+
+// // noCanvas();
+//     // img = loadImage('face.png');
+
+//     // var face = document.querySelector("#face");
+//     // face.addEventListener("click", function(){
+//     //     createCanvas(500,500);
+
+//     //     background(0);
+//     // })
+
+//     // face.class("#face");
+    
+
+// // function draw(){
+// //     image(img)
+// // }
+
+
+// // var qFoundation = document.querySelector('.foundation');
+//     // var qBlush = document.querySelectorAll('.blush');
+//     // var qEyeshadow = document.querySelectorAll('.eyeshadow');
+//     // var qEyeliner = document.querySelectorAll('.eyeliner');
+
+//     // if(qFoundation){
+//     //     foundation = select('#foundation', qFoundation);
+//     //     foundation.mousePressed();
+
+//     // }
+
+
+
+//     // var faceClick = createA("#face", "face");
+
+//     // var face = document.querySelector("#face");
+//     // face.addEventListener("click", function(){
+//     //     createCanvas(500,500);
+//     //     background(0);
+
+//     // })
+
+//     // faceClick.class("face");
+
+
+
+  
+
+
+// // function addPhoto(){
+// //     var img = createImage('face.png');
+// //     img.class('face');
+// //     img.size(100,100);
+// //     console.log("added image")
+// // }
+
+// // addPhoto();

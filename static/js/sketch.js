0d34f3eba0b990692fde7af01a45144910393f8e
Jeffrey Burgos <jeffreyb92@gmail.com>
2020-03-13 23:38:47 -0400

    fixed css link in index, changed some css, working on sketch

--- static/js/sketch.js
+++ static/js/sketch.js
@@ -3,34 +3,34 @@
 //image if the value is less than .5 = 0 stars
 //if > .5 then its 1 and so forth
 function ratingRendering(){
+
     d3.json("../makeup_data.json").then((data) => {
         console.log(data);
         
         var foundationRating = [];
-        var blushRating = [];
-        var eyeshadowRating = [];
-        var eyelinerRating = [];
+        let topFoundationRating = data["top10_foundation_rating.csv"];
 
-        let topFoundationRating = data["top10_foundation_rating"];
+        // let foundation = d3.selectAll("[alt='foundation']");
+        for(const foundation in topFoundationRating) {
+            var foundationthings = topFoundationRating[foundation]
+            for(const rating in foundationthings) {
+                if (rating == "rating") {
+                    foundationRating.push(foundationthings[rating]);
+                    }
+                }
+        }
 
-        let foundation = d3.selectAll("[alt='foundation']");
-        for(const f in topFoundationRating){
-            var foundationProdRating = topFoundationRating[f];
-            for(const rating in foundationProdRating){
-                if (rating == "rating"){
-                    foundationRating.push(foundationProdRating[rating]);
-                };
-            };
-        };
-
+          console.log(foundationRating);
     });
-    console.log(foundationRating);
+
+  
 
     d3.json("../makeup_data.json").then((data) => {
 
         let topBlushRating = data["top10_blush_rating"];
+        var blushRating = [];
 
-        let blush = d3.selectAll("[alt='blush']");
+        // let blush = d3.selectAll("[alt='blush']");
         for(const b in topBlushRating){
             var blushProdRating = topBlushRating[b];
             for(const rating in blushProdRating){
@@ -47,6 +47,7 @@ function ratingRendering(){
     d3.json("../makeup_data.json").then((data) => {
 
         let topEyeshadowRating = data["top10_eyeshadow_rating"];
+        var eyeshadowRating = [];
 
         let eyeshadow = d3.selectAll("[alt='eyeshadow']");
         for(const e in topEyeshadowRating){
@@ -65,6 +66,7 @@ function ratingRendering(){
     d3.json("../makeup_data.json").then((data) => {
 
         let topEyelinerRating = data["top10_eyeliner_rating"];
+        var eyelinerRating = [];
 
         let eyeliner = d3.selectAll("[alt='eyeliner']");
         for(const e in topEyelinerRating){
